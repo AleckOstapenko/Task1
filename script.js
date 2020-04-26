c0a94a7427ca2737630a648a119eb14ee1c6aa76
Aleksandr <aleck.ostapenko@gmail.com>
2020-04-26 22:18:04 +0300

    First task

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+let money, time;
+money = prompt("Ваш бюджет на месяц?");
+time = prompt("Введите дату в формате YYYY-MM-DD");
+
+let appdata = {
+    budget : money,
+    timeData : time,
+    expenses : {},
+    optionalExpences : {},
+    income : [],
+    saving : false
+};
+
+let expensesName=prompt("Введите обязательную статью расходов в этом месяце");
+let expencesMoney=+prompt("Во сколько обойдется?");
+appdata.optionalExpences[expensesName]=expencesMoney;
+
+alert("Бюджет на 1 день "+(appdata.budget/30));
